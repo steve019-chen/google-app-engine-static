81b34035ff21768852313ad3d0e3fb169e76cef7
Karl-Heinz Müller <karlheinz@gmail.com>
2020-08-21 12:36:51 -0400

    Create pages.py

---
+++ pages.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+"""
+    A dictionary mapping request path with the corresponding template.
+
+    See pending.MD for upcoming content
+
+"""
+documents = {}
+documents['/'] = { 'template' : '/home.html', 'published': '2020-01-01', 'modified': '2020-01-01', 'priority': '1.0' }
